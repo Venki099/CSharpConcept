d520546d57b8b0ecca6dfc1d847a7ee05e79f5fa
Venki099 <vsumukh1996@gmail.com>
2022-04-17 09:10:31 +0530

    Trainee_prog

---
+++ CSharpConcept/Trainee.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSharpConcept
+{
+    internal class Trainee
+    {
+        public static void Main()
+        {
+            int ID;
+            string TraineeName;
+            string TraineeDescription;
+
+            Console.WriteLine("Enter the Employee ID");
+
+            ID = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Enter Employee name");
+            TraineeName = Console.ReadLine();
+            Console.WriteLine("Enter Employee Des");
+            TraineeDescription = Console.ReadLine();
+
+            Console.WriteLine($"The Employee Id is {ID}\n Trainee Name is {TraineeName}\n Trainee Description is {TraineeDescription}");
+        }
+    }
+}
